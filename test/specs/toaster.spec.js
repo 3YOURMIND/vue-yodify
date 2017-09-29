d8ba46036caa26448a2bb69232399160df3705d3
Kevin Peters <kpe@3yourmind.com>
2017-09-29 11:15:00 +0200

    refactor(rewrite): remove all files

--- test/specs/toaster.spec.js
+++
@@ -1,9 +0,0 @@
-import Toaster from 'src/toaster.vue'
-import { createVM } from '../helpers/utils.js'
-
-describe('Toaster.vue', function () {
-  it('should render correct contents', function () {
-    // eslint-disable-next-line
-    const vm = createVM(this, `<Toaster types="error" :autoClose="true" autoCloseTime="8000" :showToaster="true" content="Hello Meeesage"></Toaster>`, { components: { Toaster }})
-  })
-})
