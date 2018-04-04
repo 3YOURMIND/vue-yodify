fa5211dcbf2c753f2b8c63d1d9712b362a3fbdf1
Florian Wendelborn <1133858+dodekeract@users.noreply.github.com>
2018-04-04 14:16:03 +0200

    fix: CSS isn't inlined

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 /* global module, process */
 const { YODIFY_BUILD_LIBRARY } = process.env;
 
-module.exports = {
-	configureWebpack: {
-		entry: './example/main.js',
-	},
-};
-
-if (YODIFY_BUILD_LIBRARY) module.exports = {};
+if (YODIFY_BUILD_LIBRARY)
+	module.exports = {
+		css: {
+			extract: false,
+		},
+	};
+else
+	module.exports = {
+		configureWebpack: {
+			entry: './example/main.js',
+		},
+	};
