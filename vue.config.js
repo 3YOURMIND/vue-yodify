0e47bed9fbd1acd3bb8fee2934b4720b8d106569
Florian Wendelborn <1133858+dodekeract@users.noreply.github.com>
2018-04-04 13:51:44 +0200

    fix: build ignores path with custom webpack entry

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
-/* global module */
+/* global module, process */
+const { YODIFY_BUILD_LIBRARY } = process.env;
+
 module.exports = {
 	configureWebpack: {
 		entry: './example/main.js',
 	},
-}
+};
+
+if (YODIFY_BUILD_LIBRARY) module.exports = {};
